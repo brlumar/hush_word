a127fba58dcb0727e2129fb302c763cbdaee75c0
brlumar <126432304+brlumar@users.noreply.github.com>
2023-10-11 20:31:45 -0500

    added funtion to the start btn and remove two team buttons

--- js/script.js
+++ js/script.js
@@ -6,13 +6,15 @@ const buzzWord2El = document.querySelector('.buzzword2');
 const buzzWord3El = document.querySelector('.buzzword3');
 const buzzWord4El = document.querySelector('.buzzword4');
 const timeElement = document.getElementById('time'); //variable that gives access to the clock in the DOM 
-const startOne = document.getElementById('team-one-button'); //points to team one start button
-const startTwo = document.getElementById('team-two-button'); //points to team two start button
+// const startOne = document.getElementById('team-one-button'); //points to team one start button
+// const startTwo = document.getElementById('team-two-button'); //points to team two start button
 const audioElBuzzer = document.getElementById('buzzer');
 const audioEltimer = document.getElementById('end-turn');
 const audioElCorrect = document.getElementById('correct');
 const audioElTimerEnd = document.getElementById('timer-running');
 const audioElAww = document.getElementById('aww');
+const playarea = document.getElementById('playarea');
+const startBtn = document.getElementById('start-btn');
 
 
 let scoreOneDisplay = document.querySelector('.team-one-score'); //points to team one display in the DOM
@@ -25,43 +27,82 @@ let time = 0; //seconds allowed per turn
 let timerInterval; //variable for the clock interval
 let teamScoreOne = 0; //holds the score for team one
 let teamScoreTwo = 0; //holds the score for team two
-let whoseTurn = 0;
+let whoseTurn = 1;
 
 let cardModal = document.getElementById('card-modal');
 
 
 
-function startTurnOne() {
-if(time == 0){
-    clearCard();
-    updateCard();
-    startTimer();
-    time = 60; //resets the timer to 60 seconds
-    clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
+function startTurn() {
+    if (whoseTurn == 1) {
+        if (time == 0) {
+            clearCard();
+            updateCard();
+            startTimer();
+            time = 60; //resets the timer to 60 seconds
+            clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
 
-    timerInterval = setInterval(updateTimer, 1000);
+            timerInterval = setInterval(updateTimer, 1000);
 
-    whoseTurn = 1;
-    console.log("It's team ", whoseTurn, " turn.");
-    print();
-}else{
-    playTimerRunning();
+            // whoseTurn = 1;
+            console.log("It's team ", whoseTurn, " turn.");
+            print();
+        } else {
+            playTimerRunning();
+
+        }
+    } else if (whoseTurn = 2) {
+        if (time == 0) {
+            clearCard();
+            updateCard();
+            startTimer();
+            time = 60; //resets the time to 60 seconds 
+            clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
+
+            timerInterval = setInterval(updateTimer, 1000);
+            // whoseTurn = 2;
+            console.log("It's team ", whoseTurn, " turn.");
+            print();
+        } else {
+            playTimerRunning();
+        }
+
+
+    }
 }
+
+function startTurnOne() {
+    if (time == 0) {
+        clearCard();
+        updateCard();
+        startTimer();
+        time = 60; //resets the timer to 60 seconds
+        clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
+
+        timerInterval = setInterval(updateTimer, 1000);
+
+        whoseTurn = 1;
+        console.log("It's team ", whoseTurn, " turn.");
+        print();
+    } else {
+        playTimerRunning();
+
+    }
 }
 
 function startTurnTwo() {
-    if(time == 0){
-    clearCard();
-    updateCard();
-    startTimer();
-    time = 60; //resets the time to 60 seconds 
-    clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
+    if (time == 0) {
+        clearCard();
+        updateCard();
+        startTimer();
+        time = 60; //resets the time to 60 seconds 
+        clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
 
-    timerInterval = setInterval(updateTimer, 1000);
-    whoseTurn = 2;
-    console.log("It's team ", whoseTurn, " turn.");
-    print();
-    }else{
+        timerInterval = setInterval(updateTimer, 1000);
+        whoseTurn = 2;
+        console.log("It's team ", whoseTurn, " turn.");
+        print();
+    } else {
         playTimerRunning();
     }
 }
@@ -77,7 +118,12 @@ function updateTimer() {
         clearInterval(timerInterval);
         playBuzzer();
         playEndTurn();
-    
+        if (whoseTurn==1){
+            whoseTurn=2;
+        }else if(whoseTurn==2){
+            whoseTurn=1;
+        }
+
     } else {
         time--;
     }
@@ -92,11 +138,11 @@ function startTimer() {
 
 
 function addToScore() {
-    if(time>=0){
-    writeScore();
-    console.log('score one is: ', teamScoreOne, 'score two is: ', teamScoreTwo);
-    clearCard();
-    updateCard();
+    if (time >= 0) {
+        writeScore();
+        console.log('score one is: ', teamScoreOne, 'score two is: ', teamScoreTwo);
+        clearCard();
+        updateCard();
     }
 }
 
@@ -126,7 +172,7 @@ function writeScore() {
 
 function showCard() {
 
-    cardModal.style.display = 'flex';
+    playarea.style.display = 'flex';
 
 }
 
@@ -157,28 +203,28 @@ function updateCard() { //Function that updates the card information
 
 }
 
-function playBuzzer(){
+function playBuzzer() {
     audioElBuzzer.play();
 }
 
-function playEndTurn (){
+function playEndTurn() {
     audioEltimer.play();
 }
-function playCorrect(){
+function playCorrect() {
     audioElCorrect.play();
 }
 
-function playTimerRunning(){
+function playTimerRunning() {
     audioElTimerEnd.play();
 }
-function playAww(){
+function playAww() {
     audioElAww.play();
 }
 
-startOne.addEventListener('click', startTurnOne); //starts team one when Go button is pressed
-startTwo.addEventListener('click', startTurnTwo); //starts team two when Go button is pressed
+// startOne.addEventListener('click', startTurnOne); //starts team one when Go button is pressed
+// startTwo.addEventListener('click', startTurnTwo); //starts team two when Go button is pressed
 
 
 correctBTN.addEventListener('click', addToScore);
-
 incorrectBTN.addEventListener('click', pass);
+startBtn.addEventListener('click', startTurnOne);
